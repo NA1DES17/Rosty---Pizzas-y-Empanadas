478b2fecbf4905d2ba816e5c82a05ecfea2b2bfb
Nicolas <nosoyna1des17@gmail.com>
2024-09-19 20:51:33 -0300

    Rehacer

--- appIndex.js
+++ appIndex.js
@@ -74,28 +74,3 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 // --------------Galeria---------------
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Para el iframe del mapa
-  const mapa = document.createElement("iframe");
-  mapa.src =
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d383.288011741564!2d-58.25847210160984!3d-34.74169874733671!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32f56953773ab%3A0xedd1486102cc66d3!2sRosty%20pizzas%20y%20empanadas!5e1!3m2!1ses!2sar!4v1726093228226!5m2!1ses!2sar";
-  mapa.width = "600";
-  mapa.height = "450";
-  mapa.style.border = "0";
-  mapa.allowFullscreen = "";
-  mapa.loading = "lazy";
-  mapa.referrerPolicy = "no-referrer-when-downgrade";
-  document.querySelector("#benefits").appendChild(mapa);
-
-  // Para el iframe del video de YouTube
-  const video = document.createElement("iframe");
-  video.src = "https://www.youtube.com/embed/8X9y_49p6do?si=sBiXKzqnjKHey-xu";
-  video.className = "videoContainer";
-  video.title = "YouTube video player";
-  video.frameBorder = "0";
-  video.allow =
-    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-  video.allowFullscreen = true;
-  document.querySelector(".videoContainer").replaceWith(video);
-});
